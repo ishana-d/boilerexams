b41f8fb122a712be03f0258923253de1c4c4e5ad
ishana-d <ishana.didwania@gmail.com>
2026-01-15 10:36:26 -0500

    Fix Vercel API proxy runtime

--- src/api/question.js
+++ src/api/question.js
@@ -1,9 +1,9 @@
 export const QUESTION_IDS = [
   "d44531f1-3cf7-404d-bd10-e9a786484b8a",
   "4f041e07-ef31-44b1-944a-8b2dad3ca3de",
-  "6f672ca1-84bd-4355-8b9e-9625fda6c77a",
+  "f075fec6-61a7-41e1-9ff3-0c8f224d2413",
   "2e0e796b-8e16-41af-9f7d-634561e99248",
-  "e897c130-d534-4836-9ec4-39e4bcb0006e",
+  "d9d65ab8-afd4-4620-a441-a7d704fb8453",
 ];
 
 export async function fetchQuestion(id) {
